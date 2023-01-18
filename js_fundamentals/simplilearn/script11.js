1c0dadf1f7b39af0a661e81fb6bd366d9d7d0e37
laiszig <laisziegler@gmail.com>
2023-01-18 15:19:58 -0300

    conditional, switch

---
+++ js_fundamentals/simplilearn/script11.js
@@ -0,0 +1,20 @@
+var ourFriends = [["Laurance, Mike, David"],[4,5,7]];
+console.log(ourFriends[0][2]);
+
+var myArray = [{
+    name1:"Laurance",
+    name2:"Mike",
+    name3:"David"
+}, {
+    name1: "Tina",
+    name2: "Becca",
+    name3: "Mila"
+}];
+console.log(myArray[0].name3);
+
+var myObj = {
+    set1: ["Laurence", "Mike", "David"],
+    set2: [4, 5, 7]
+};
+
+console.log(myObj);
---
+++ js_fundamentals/simplilearn/script12.js
@@ -0,0 +1,12 @@
+var a = 5;
+var b = a;
+
+if (a < b) {
+    console.log('B is greater than A');
+} else if (a > b) {
+    console.log('A is greater than B');
+} else if (a == b) {
+    console.log('B is equal to A');
+} else {
+    console.log('Not correc or false.')
+}
